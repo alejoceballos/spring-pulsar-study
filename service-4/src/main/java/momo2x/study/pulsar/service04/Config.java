40a5ffc9f9a2d31c6b9731caa030c86648db74f4
Alejo Ceballos <alejoceballos75@gmail.com>
2025-05-06 15:33:48 +0100

    No Java Agent or Spring Boot to enable tracing in Grafana

--- service-4/src/main/java/momo2x/study/pulsar/service04/Config.java
+++ service-4/src/main/java/momo2x/study/pulsar/service04/Config.java
@@ -1,26 +1,56 @@
 package momo2x.study.pulsar.service04;
 
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.EnvironmentResourceProvider;
+import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+
+import java.util.Map;
 
 public class Config {
 
     private static OpenTelemetrySdk openTelemetrySdk;
 
-    public static void autoconfiguredSdk() {
+    public static void configuredSdk() {
         if (openTelemetrySdk == null) {
-            openTelemetrySdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+            final var resource =
+                    new EnvironmentResourceProvider()
+                            .createResource(
+                                    DefaultConfigProperties.create(Map.of()));
+
+            final var tracerProvider = SdkTracerProvider
+                    .builder()
+                    .setResource(resource)
+                    .addSpanProcessor(SimpleSpanProcessor.create(OtlpGrpcSpanExporter.getDefault()))
+                    .build();
+
+            final var contextPropagators = ContextPropagators.create(
+                    TextMapPropagator.composite(
+                            W3CTraceContextPropagator.getInstance(),
+                            W3CBaggagePropagator.getInstance()));
+
+            openTelemetrySdk =
+                    OpenTelemetrySdk.builder()
+                            .setTracerProvider(tracerProvider)
+                            .setPropagators(contextPropagators)
+                            .build();
         }
     }
 
     public static OpenTelemetrySdk getOpenTelemetrySdk() {
-        autoconfiguredSdk();
+        configuredSdk();
         return openTelemetrySdk;
     }
 
     public static Tracer getTracer() {
-        return getOpenTelemetrySdk().getTracer("test");
+        return getOpenTelemetrySdk().getTracer("momo2x.study.pulsar");
     }
 
 }
--- service-4/src/main/java/momo2x/study/pulsar/service04/Main.java
+++ service-4/src/main/java/momo2x/study/pulsar/service04/Main.java
@@ -4,6 +4,7 @@ import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.slf4j.Logger;
 
+import static java.lang.Runtime.getRuntime;
 import static momo2x.study.pulsar.service04.Utils.SERVICE_URL;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -16,7 +17,7 @@ public class Main {
         log.info("-> [Env] OTEL_SERVICE_NAME: \"{}\"", System.getenv("OTEL_SERVICE_NAME"));
 
         log.info(" --> Configuring OpenTelemetry SDK");
-        Config.autoconfiguredSdk();
+        final var openTelemetry = Config.getOpenTelemetrySdk();
         log.info(" --> OpenTelemetry SDK configured");
 
         try (final var pulsarClient =
@@ -36,6 +37,8 @@ public class Main {
         } finally {
             log.info(" --> Finishing the service execution");
         }
+
+        getRuntime().addShutdownHook(new Thread(openTelemetry::close));
     }
 
 }
